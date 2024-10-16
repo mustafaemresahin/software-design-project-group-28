ef0d9a22e43acd38ea9918687c148807e61bcbde
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-10-16 00:23:59 -0500

    event model

---
+++ models/Event.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  eventName: { type: String, required: true },
+  eventDescription: { type: String, required: true },
+  location: { type: String, required: true },
+  requiredSkills: [{ type: String, required: true }],
+  urgency: { type: String, required: true },
+  eventDate: { type: Date, required: true }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
